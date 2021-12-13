5e7b5f2982872a2fc705f27efb5e8a07aa13ba48
Tyler Kirkpatrick <tyl3rkirkpatrick@yahoo.com>
2021-12-13 14:44:45 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,229 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState} from 'react';
+//import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import './App.css'
+import {  
+        getTopMessagersByThreshold,
+        getTopMessagersByPercent,
+        getTopMessagersByTotalLikes,
+        getTopMessagersByTotalMessages 
+      } 
+  from './components/loadGroupMeData'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    title: {
+      display: true,
+      text: 'GroupMe Messaging Data',
+    },
+  },
+};
 
 function App() {
+
+  const returnLabelsArray = (messagerArray) => {
+    let namesArray = []
+    for(let messager of messagerArray) {
+      namesArray.push(messager.name)
+    }
+    return namesArray
+  }
+  
+  const returnData = (messagerArray) => {
+    if(messagerArray && messagerArray.length > 0) {
+      let whichCategoryString = ""
+      if(whichCategory === 0) {
+        whichCategoryString = "topMessages"
+      } else if (whichCategory === 1) {
+        whichCategoryString = "percentOfMessagesThatMetThreshold"
+      } else if (whichCategory === 2) {
+        whichCategoryString = "totalLikes"
+      } else if(whichCategory === 3) {
+        whichCategoryString = "totalMessages"
+      } else {
+        whichCategoryString = "data"
+      }
+      
+      const labels = returnLabelsArray(messagerArray)
+      const data = messagerArray.map((messager) => messager[whichCategoryString])
+    
+      return {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Dataset 1',
+            data: data,
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          },
+        ],
+      };
+    } else {
+      return {
+        labels: [],
+        datasets: []
+      }
+    }
+  }
+
+  const handleChange = (event) => {
+    setThreshold(event.target.value);
+  };
+
+  const handleChangeCategory = (event) => {
+    setWhichCategory(event.target.value);
+  }
+
+  // eslint-disable-next-line
+  const [ topMessagers, setTopMessagers ] = useState([])
+  // eslint-disable-next-line
+  const [labels, setLabels] = useState({})
+  const [whichCategory, setWhichCategory] = useState(0)
+  const [threshold, setThreshold] = useState(20)
+  const [data, setData] = useState({
+    labels: [],
+    datasets: []
+  })
+
+  useEffect(() => {
+    let yeet = null
+    if(whichCategory === 0) {
+      yeet = getTopMessagersByThreshold(25, threshold)
+    } else if (whichCategory === 1) {
+      yeet = getTopMessagersByPercent(25, threshold)
+    } else if (whichCategory === 2) {
+      yeet = getTopMessagersByTotalLikes(25, 0)
+    } else if (whichCategory === 3) {
+      yeet = getTopMessagersByTotalMessages(25, 0)
+    } else if (whichCategory === 4) {
+      yeet = [
+        {
+          name: "Mike Spencer",
+          data: 1,
+        },
+        {
+          name: "Everyone Else",
+          data: 0
+        }
+      ]
+    } else if (whichCategory === 5) {
+      yeet = [
+        {
+          name: "Tyler Kirkpatrick",
+          data: 3,
+        },
+        {
+          name: "Mike Spencer",
+          data: 3
+        },
+        {
+          name: "Daniel Liñan",
+          data: 2
+        },
+        {
+          name: "Ryan Pool",
+          data: 2
+        },
+        {
+          name: "Tanner Mauro",
+          data: 2
+        },
+        {
+          name: "John Lazar",
+          data: 2
+        },
+        {
+          name: "Brandon Campbell",
+          data: 2
+        },
+        {
+          name: "Brandon Carlton",
+          data: 1
+        },
+        {
+          name: "Haywood Miller",
+          data: 0
+        },
+      ]
+    }
+    setTopMessagers(yeet)
+    setLabels(returnLabelsArray(yeet))
+    setData(returnData(yeet))
+  // eslint-disable-next-line
+  }, [threshold, whichCategory])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+
+      <h3>Mays MBA Class of 2022 GroupMe Data</h3>
+
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-category-label">Category</InputLabel>
+        <Select
+          labelId="demo-simple-select-category-label"
+          id="demo-simple-select-category"
+          value={whichCategory}
+          label="Category"
+          onChange={handleChangeCategory}
+          
         >
-          Learn React
-        </a>
-      </header>
+          <MenuItem value={0}>Most Messages Meeting Liked Threshold</MenuItem>
+          <MenuItem value={1}>Highest Percent of Messages Meeting Liked Threshold</MenuItem>
+          <MenuItem value={2}>Total Likes From Messages</MenuItem>
+          <MenuItem value={3}>Most Sent Messages</MenuItem>
+          <MenuItem value={4}>Most Canadian Messages</MenuItem>
+          <MenuItem value={5}>Most WarZone Dubs During Class</MenuItem>
+        </Select>
+        
+      </FormControl>
+      <br />
+      <br />
+      <br />
+      { whichCategory >= 2 ? null : ( 
+      <FormControl fullWidth>
+      
+            <InputLabel id="demo-simple-select-threshold-label">Likes Threshold</InputLabel>
+            <Select
+              labelId="demo-simple-select-threshold-label"
+              id="demo-simple-select-threshold"
+              value={threshold}
+              label="Likes Threshold"
+              onChange={handleChange}
+            >
+              <MenuItem value={10}>10</MenuItem>
+              <MenuItem value={15}>15</MenuItem>
+              <MenuItem value={20}>20</MenuItem>
+              <MenuItem value={25}>25</MenuItem>
+              <MenuItem value={30}>30</MenuItem>
+            </Select>
+
+      </FormControl>
+      )}
+      <Bar options={options} data={data} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/loadGroupMeData.js
@@ -0,0 +1,179 @@
+const rawData = require('./00001/59528655/message.json')
+const groupMemberData = require('./00001/59528655/conversation.json')
+
+const getGroupMembers = () => {
+    //get all groupParticipants with their name and ID's
+    //return JSON.parse(groupMemberData)
+    return groupMemberData
+}
+
+const getConversation = () => {
+    //return JSON.parse(rawData)
+    return rawData
+}
+
+const getDictionaryOfIdsToNames = () => {
+    const dict = getGroupMembers()
+    const membersArray = dict['members']
+    const idNameDict = {}
+    for(let personObject of membersArray) {
+        const user_id = personObject['user_id']
+        const user_name = personObject['nickname']
+        idNameDict[user_id] = user_name
+    }
+    return idNameDict
+}
+
+const convertIdsToNames = (arrayOfArrays) => {
+    //console.log('arrayOfArrays: ', arrayOfArrays)
+    const topMembers = arrayOfArrays
+    const TopTenDictWithNames = {}
+    const dictOfIdsToNames = getDictionaryOfIdsToNames()
+    for(let member of topMembers) {
+        const memberName = dictOfIdsToNames[member[0]]
+        TopTenDictWithNames[memberName] = member[1]
+    }
+    return TopTenDictWithNames
+}
+
+const getMostActiveMembersFromDict = (dict, numberOfEntries) => {
+    let topTenArray = []
+    
+
+    for (var user_id in dict) {
+        topTenArray.push([user_id, dict[user_id]]);
+    }
+
+    topTenArray = topTenArray.sort(function(a, b) {
+        return a[1] - b[1];
+    })
+
+    topTenArray = topTenArray.splice(topTenArray.length - numberOfEntries, numberOfEntries)
+
+    const sortedMostActiveMembers = convertIdsToNames(topTenArray)
+    return sortedMostActiveMembers
+
+}
+// eslint-disable-next-line
+const printMostActiveMembers = (numberOfEntries) => {
+    const conv = getConversation()
+    let dict = {}
+    for(let message of conv) {
+        const sender_id = message['sender_id']
+        if(dict[sender_id] === undefined) {
+            dict[sender_id] = 1
+        } else {
+            dict[sender_id] += 1
+        }
+    }
+
+    return getMostActiveMembersFromDict(dict, numberOfEntries)
+    
+}
+
+const generateNamesAndScoreArray = (likesThreshold) => {
+    
+    const dict = getConversation()
+
+    const dictOfIds = getDictionaryOfIdsToNames()
+
+    let mostLikedMessages = {}
+    let totalMessagesCount = {}
+    let totalLikesCount = {}
+
+    //console.log('dict: ', dict[0])
+    for(let message of dict) {
+        //console.log('message: ', message)
+
+        if(message.sender_id) {
+            const senderName = dictOfIds[message.sender_id]
+
+            if(!totalMessagesCount[senderName])
+                totalMessagesCount[senderName] = 0
+            totalMessagesCount[senderName] += 1
+
+            if(!totalLikesCount[senderName])
+                totalLikesCount[senderName] = 0
+
+            if(message.favorited_by && message.favorited_by.length) {
+                totalLikesCount[senderName] += message.favorited_by.length
+            }
+        }        
+
+        if(message.favorited_by && message.favorited_by.length > likesThreshold) {
+            
+            const senderName = dictOfIds[message.sender_id]
+            
+            if(!mostLikedMessages[senderName])
+                mostLikedMessages[senderName] = []
+            if(message.text) {
+                //console.log('message: ', message.text)
+                mostLikedMessages[senderName].push(message.text)
+            } else if(message.attachments && message.attachments[0] && message.attachments[0].url ) {
+                //console.log('attachments: ', message.attachments)
+                mostLikedMessages[senderName].push(message.attachments[0].url)
+            }
+        }
+    }
+    //console.log('mostLikedMessages: ', mostLikedMessages)
+
+    const namesToTopMessages = {}
+    var namesAndScoreArray = []
+
+    for(let name of Object.keys(mostLikedMessages)) {
+        namesToTopMessages[name] = { 
+            topMessages: mostLikedMessages[name].length,
+            topMessageThreshold: likesThreshold,
+            totalMessages: totalMessagesCount[name],
+            percentOfMessagesThatMetThreshold: mostLikedMessages[name].length/totalMessagesCount[name]
+        }
+        namesAndScoreArray.push({
+            name: name,
+            topMessages: mostLikedMessages[name].length,
+            topMessageThreshold: likesThreshold,
+            totalMessages: totalMessagesCount[name],
+            totalLikes: totalLikesCount[name],
+            percentOfMessagesThatMetThreshold: mostLikedMessages[name].length/totalMessagesCount[name]
+        })
+    }    
+
+    return namesAndScoreArray
+}
+
+export function getTopMessagersByThreshold(numberToRetrieve, likesThreshold) {
+    //sort by topMessage count
+    const namesAndScoreArray = generateNamesAndScoreArray(likesThreshold)
+    return namesAndScoreArray.sort(function (a, b) {
+        return b.topMessages - a.topMessages;
+    }).slice(0,numberToRetrieve);
+}
+
+export function getTopMessagersByPercent(numberToRetrieve, likesThreshold) {
+    //sort by topMessage percent
+    const namesAndScoreArray = generateNamesAndScoreArray(likesThreshold)
+    return namesAndScoreArray.sort(function (a, b) {
+        return b.percentOfMessagesThatMetThreshold - a.percentOfMessagesThatMetThreshold;
+    }).slice(0,numberToRetrieve);
+}
+
+export function getTopMessagersByTotalLikes(numberToRetrieve, likesThreshold) {
+    //sort by topMessage percent
+    const namesAndScoreArray = generateNamesAndScoreArray(0)
+    return namesAndScoreArray.sort(function (a, b) {
+        return b.totalLikes - a.totalLikes;
+    }).slice(0,numberToRetrieve);
+}
+
+export function getTopMessagersByTotalMessages(numberToRetrieve, likesThreshold) {
+    //sort by topMessage percent
+    const namesAndScoreArray = generateNamesAndScoreArray(0)
+    return namesAndScoreArray.sort(function (a, b) {
+        return b.totalMessages - a.totalMessages;
+    }).slice(0,numberToRetrieve);
+}
+
+//const arr = generateNamesAndScoreArray(3, 15)
+//const topSendersByThreshold = getTopMessagersByThreshold(arr)
+
+//const topSendersByPercent = getTopMessagersByPercent(arr)
+//console.log('topSendersByPercent: ', topSendersByPercent)
